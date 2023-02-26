893ad5c569306052632fcdeddb8cb5281076f575
Jean-Louis Bionville <jlbionville@alfaco.fr>
2023-02-26 11:08:45 +0100

    code pour charger les settings du package

---
+++ AlfacoSetting.py
@@ -0,0 +1,8 @@
+import sublime
+
+# Charge les paramètres du fichier Preferences.sublime-settings
+alfacoSettings = sublime.load_settings('alfaco.sublime-settings')
+
+# Définit les variables globales pour les paramètres
+# my_setting = settings.get('my_setting')
+# my_other_setting = settings.get('my_other_setting')
