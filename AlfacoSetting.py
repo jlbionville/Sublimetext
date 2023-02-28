81ed5951252a413c807cc0d8e4aef8564f425503
Jean-Louis Bionville <jlbionville@alfaco.fr>
2023-02-28 16:09:19 +0100

    suite regroupement plugin dans un fichier

--- AlfacoSetting.py
+++
@@ -1,8 +0,0 @@
-import sublime
-
-# Charge les paramètres du fichier Preferences.sublime-settings
-alfacoSettings = sublime.load_settings('alfaco.sublime-settings')
-
-# Définit les variables globales pour les paramètres
-# my_setting = settings.get('my_setting')
-# my_other_setting = settings.get('my_other_setting')
