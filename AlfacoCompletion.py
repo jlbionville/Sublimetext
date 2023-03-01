4d0e1fb10e311474e987a89ee2b61295b7aacaa3
Jean-Louis Bionville <jlbionville@alfaco.fr>
2023-03-01 08:56:27 +0100

    exemple de completion

---
+++ AlfacoCompletion.py
@@ -0,0 +1,25 @@
+import sublime
+import sublime_plugin
+
+
+class AlfacoCompletion(sublime_plugin.EventListener):
+    def on_query_completions(self, view, prefix, locations):
+        if not view.match_selector(locations[0], "source.python"):
+            return []
+
+        available_completions = [
+            "def",
+            "class",
+            "None",
+            "True",
+            "False"
+        ]
+
+        prefix = prefix.lower()
+
+        out = []
+        for comp in available_completions:
+            if comp.lower().startswith(prefix):
+                out.append(comp)
+
+        return out
