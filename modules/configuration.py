39fc6fbd1d44f325975fc59f9b43fbbc1d88e123
Jean-Louis Bionville <jlbionville@alfaco.fr>
2023-03-03 09:54:44 +0100

    move code to plugin file

--- modules/configuration.py
+++ modules/configuration.py
@@ -6,16 +6,6 @@ def addSetting(settings):
 	'''
 	print('dans le addSetting {}'.format(settings.get('jira')))
 
-def getSetting(key):
-	'''
-	charge les différents fichiers settings nécessaires pour le package
-	renvoie un setting qui est une aggrégation des différents fichiers
-	'''
-	if settings_alfaco.has(key):
-		return settings_alfaco.get(key)
-	if settings_sublime.has(key):
-		return settings_sublime.get(key)
-	if settings_atlassian.has(key):
-		return settings_atlassian.get(key)
+
 
 	
