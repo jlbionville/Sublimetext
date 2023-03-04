50a81312d270fb77d3b6f8471f8d30952648c2d7
Jean-Louis Bionville <jlbionville@alfaco.fr>
2023-03-04 13:00:54 +0100

    ajout demethode pour obtenir les informations de la configuraiton

--- modules/configuration.py
+++ modules/configuration.py
@@ -19,11 +19,28 @@ class Configuration(object):
 	listeKeyJiraProject=[]
 	def __init__(self):
 		super(Configuration, self).__init__()
-	
-		
-
-
-
-
+	# def __init__(self,dictionnaire):
+	# 	super(Configuration, self).__init__()	
+	# 	dictionnary=dictionnaire
+	# def __init__(self,organisation,project):
+	# 	super(Configuration, self).__init__()	
+	# 	dictionnary=dictionnaire
+	# 	__organisation__=organisation
+	# 	__jira_project__=project
+	def setKeyValue(self,key,value):
+		self.dictionnary[key]=value
+	def getKeyValue(self,key):
+		return self.dictionnary[key]
+	def setJiraAuthorisation(self,login,password):
+		self.dictionnary["jira"]["login"]=login
+		self.dictionnary["jira"]["password"]=password
+	def getJiraAuthorisation(self,):
+		return (self.dictionnary["jira"]["login"], self.dictionnary["jira"]["password"])
+	def getBaseUrlForRESTApi(self,):
+		return 'https://{}.atlassian.net/rest/api/latest/'.format(self.getKeyValue("default_organisation"))
+	def setListKeyJiraProject(self,liste):
+		self.listeKeyJiraProject=liste
+	def getListKeyJiraProject(self):
+		return self.listeKeyJiraProject
 
 	
