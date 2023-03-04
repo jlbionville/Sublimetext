2cade92459bfcb21807bd23360f4caadb531b695
Jean-Louis Bionville <jlbionville@alfaco.fr>
2023-03-04 13:00:22 +0100

    utilisation d'un dictionnaire pour les clefs de configuration

--- modules/configuration.py
+++ modules/configuration.py
@@ -5,6 +5,18 @@ class Configuration(object):
 	"""docstring for Configuration"""
 	__organisation__=""
 	__jira_project__=""
+	dictionnary={
+	    "jira":
+	    {
+	        "organisation_key": "",
+	        "project_key": "",
+	        "password": "",
+	        "login": ""
+	    },
+	    "headers": {'Content-type': 'application/json;charset=UTF-8','Accept': 'application/json'},
+
+	}
+	listeKeyJiraProject=[]
 	def __init__(self):
 		super(Configuration, self).__init__()
 	
